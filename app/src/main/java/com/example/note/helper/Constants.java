2ea97e33880e79a01596c21c21a77bdba0ccd626
Galal <ga71387@gmail.com>
2021-02-15 20:19:53 -0500

    Background Note

--- app/src/main/java/com/example/note/helper/Constants.java
+++ app/src/main/java/com/example/note/helper/Constants.java
@@ -1,8 +1,11 @@
 package com.example.note.helper;
 
+import android.widget.LinearLayout;
+
 import com.example.note.entities.Note;
 
 public class Constants {
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+
 }
